ad07363a67c2d01161cd8610afe770983b2cae03
Rafik <s94rfarh@uni-bonn.de>
2024-12-06 21:49:31 +0100

    cleaned and flitered the data

--- filtering_csv.py
+++ filtering_csv.py
@@ -9,7 +9,18 @@ columns_to_keep = ["Player", "Nation", "Pos", "Age", "MP", "Starts", "Min", "Gls
 # Nur die gewünschten Spalten auswählen
 df = df[columns_to_keep]
 
+#initialise empty cells
+df = df.where(pd.notnull(df), 'nan')
+
+
+# Bereinigung der 'Age'-Spalte (Text nach '-' entfernen)
+df['Age'] = df['Age'].str.split('-').str[0]
+
+df['MP'] = df['MP'].astype(str)  # Zuerst in Strings umwandeln
+df['MP'] = df['MP'].str.split('.').str[0]  # Dann den Punkt entfernen
+
+df['Age'] = df['Age'].astype(str)  # Zuerst in Strings umwandeln
+df['Age'] = df['Age'].str.split('.').str[0]  # Dann den Punkt
+
 # Änderungen in eine neue Datei speichern
 df.to_csv("filtered_stats.csv", index=False)
-
-
